d3bb35bb16fafdb09701ace80b6995d997f49de9
Pedro Dias <pdcmpa@gmail.com>
2024-12-24 12:03:46 -0300

    Renew gpg keys in update script

--- scripts/00-update.sh
+++ scripts/00-update.sh
@@ -1,3 +1,11 @@
+# update google's keyring
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --yes --dearmor -o /usr/share/keyrings/cloud.google.gpg
+
+# update kubernete's keyring and minor version
+minor_version=1.32
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v$minor_version/deb/Release.key | sudo gpg --yes --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$minor_version/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list > /dev/null
+
 # update system apps
 sudo apt update && sudo apt upgrade -y
 sudo snap refresh
